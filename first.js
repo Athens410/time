2837c41da42240a0c8fca9179c34a9545c10e8d7
Athens410 <athenah.coucean@gmail.com>
2023-05-23 13:40:24 -0400

    commit

---
+++ first.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const First = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <Red />
+    </div>
+  );
+};
+
+export default First;
---
+++ red.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MyComponent() {
+  return (
+    <div>
+      <h1>This is my first React component!</h1>
+      <p>Welcome to React!</p>
+    </div>
+  );
+}
+
+export default MyComponent;
